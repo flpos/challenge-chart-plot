76dc6040712a061db6f29481929b15d1da1f1a55
Felipe Oliveira <fsousa@tasken.com>
2021-07-31 17:34:56 -0300

    test: refactoring chartData.test.ts

--- src/chart/chartData.test.ts
+++ src/chart/chartData.test.ts
@@ -2,59 +2,63 @@ import { DataEvent, SpanEvent, StartEvent } from '../event/Event';
 import { ChartData, ChartLimits, getChartData, processSpan } from './chartData';
 
 describe('chartData functions', () => {
-  describe('handling start event', () => {
-    it('gets the min and max values', () => {
-      const input: SpanEvent = {
-        type: 'span',
-        begin: 123,
-        end: 456,
-        timestamp: 789,
-      };
-      const expected: ChartLimits = {
-        type: 'linear',
-        min: 123,
-        max: 456,
-      };
+  it('gets the min and max values', () => {
+    const input: SpanEvent = {
+      type: 'span',
+      begin: 123,
+      end: 456,
+      timestamp: 789,
+    };
+    const expected: ChartLimits = {
+      type: 'linear',
+      min: 123,
+      max: 456,
+    };
+    const noSpanExpected: ChartLimits = {
+      type: 'linear',
+      min: 'auto',
+      max: 'auto',
+    };
 
-      const result = processSpan(input);
-      expect(result.type).toBe(expected.type);
-      expect(result.min).toBe(expected.min);
-      expect(result.max).toBe(expected.max);
-    });
+    const result = processSpan(input);
+    expect(result).toEqual(expected);
 
-    it('generates the series', () => {
-      const startEvent: StartEvent = {
-        type: 'start',
-        timestamp: 1519780251293,
-        select: ['min_response_time', 'max_response_time'],
-        group: ['os', 'browser'],
-      };
+    const noSpanResult = processSpan();
+    expect(noSpanResult).toEqual(noSpanExpected);
+  });
 
-      const dataEvents: Array<DataEvent> = [
-        {
-          type: 'data',
-          timestamp: 1519780251000,
-          os: 'linux',
-          browser: 'chrome',
-          min_response_time: 0.1,
-          max_response_time: 1.3,
-        },
-      ];
+  it('generates the series', () => {
+    const startEvent: StartEvent = {
+      type: 'start',
+      timestamp: 1519780251293,
+      select: ['min_response_time', 'max_response_time'],
+      group: ['os', 'browser'],
+    };
 
-      const expected: Array<ChartData> = [
-        {
-          id: 'Linux Chrome Min Response Time',
-          data: [{ x: 1519780251000, y: 0.1 }],
-        },
-        {
-          id: 'Linux Chrome Max Response Time',
-          data: [{ x: 1519780251000, y: 1.3 }],
-        },
-      ];
+    const dataEvents: Array<DataEvent> = [
+      {
+        type: 'data',
+        timestamp: 1519780251000,
+        os: 'linux',
+        browser: 'chrome',
+        min_response_time: 0.1,
+        max_response_time: 1.3,
+      },
+    ];
 
-      const result = getChartData(startEvent, dataEvents);
-      expectArrayEquivalence<ChartData>(result, expected);
-    });
+    const expected: Array<ChartData> = [
+      {
+        id: 'Linux Chrome Min Response Time',
+        data: [{ x: 1519780251000, y: 0.1 }],
+      },
+      {
+        id: 'Linux Chrome Max Response Time',
+        data: [{ x: 1519780251000, y: 1.3 }],
+      },
+    ];
+
+    const result = getChartData(startEvent, dataEvents);
+    expectArrayEquivalence<ChartData>(result, expected);
   });
 });
 
