c72febb3bc7546620ff72f315d5dbbc1f43f4bcb
Felipe Oliveira <fsousa@tasken.com>
2021-07-31 18:07:03 -0300

    feat: Series validation Errors

---
+++ src/errors/DataValidationError.ts
@@ -0,0 +1,5 @@
+export class DataValidationError extends Error {
+  constructor(message?: string) {
+    super(message);
+  }
+}
---
+++ src/errors/SerieValidationError.ts
@@ -0,0 +1,5 @@
+export class SerieValidationError extends Error {
+  constructor(message?: string) {
+    super(message);
+  }
+}
