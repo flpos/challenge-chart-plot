29e62a3ed3c260d246cdad0952c821d43dbeee5b
Felipe Oliveira <fsousa@tasken.com>
2021-08-01 20:27:44 -0300

    style: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { HeaderWrapper } from './styles';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Felipe's Challenge</h1>;
-    </div>
+    <HeaderWrapper>
+      <h1>Felipe's Challenge</h1>
+    </HeaderWrapper>
   );
 };
 
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+  background-color: lightgray;
+  display: flex;
+  align-items: center;
+
+  color: #124;
+
+  > * {
+    width: 1200px;
+    margin: 0 auto;
+  }
+`;
