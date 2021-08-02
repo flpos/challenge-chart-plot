494fa9dbf5c5f48edb180ce0406c4f7e04fe5445
Felipe Oliveira <fsousa@tasken.com>
2021-08-01 23:05:58 -0300

    feat: add drag handle

---
+++ src/components/DragHandle/DragHandle.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { DragHandleDecorator, DragHandleDiv } from './styles';
+
+type DragHandleProps = {
+  inputHeight: number;
+  setInputHeight: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const DragHandle: React.FC<DragHandleProps> = ({
+  setInputHeight,
+  inputHeight,
+}) => {
+  const initDragRef = React.useRef(0);
+  const initHeightRef = React.useRef(0);
+
+  return (
+    <DragHandleDiv
+      draggable='true'
+      onDragStart={(event) => {
+        initDragRef.current = event.pageY;
+        initHeightRef.current = inputHeight;
+        event.dataTransfer.setDragImage(new Image(), 0, 0);
+      }}
+      onDrag={(event) => {
+        const delta = event.pageY - initDragRef.current;
+        setInputHeight((prev) => {
+          const newHeight = initHeightRef.current + delta;
+          if (newHeight > 400 || newHeight < 100) return prev;
+          return newHeight;
+        });
+      }}
+    >
+      <DragHandleDecorator />
+    </DragHandleDiv>
+  );
+};
+
+export default DragHandle;
