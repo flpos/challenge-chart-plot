c495d7287294a62eb6beea724f700722afb8e144
Felipe Oliveira <fsousa@tasken.com>
2021-08-02 21:17:27 -0300

    refactor; adjusting eventStringParse types

--- src/utils/eventStringParse.ts
+++ src/utils/eventStringParse.ts
@@ -1,6 +1,6 @@
-export const eventStringParse = (
-  text: string
-): Array<Record<string, string | number>> => {
+import { Event } from '../event/Event';
+
+export const eventStringParse = (text: string): Array<Event> => {
   const lines = text.split('\n');
   return lines.map((line) => {
     // eslint-disable-next-line no-eval
